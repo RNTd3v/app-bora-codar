09daf2f798a0c7541ffd5b3fd15706ab04f312f3
Renato Andrade <renato@rntdesign.com.br>
2020-12-20 15:04:18 -0300

    Criacao componente de formulario

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PaginationComponent } from './components/pagination/pagination.component';
 import { TableComponent } from './components/table/table.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
-  declarations: [PaginationComponent, TableComponent],
+  declarations: [PaginationComponent, TableComponent, FormComponent],
   exports: [PaginationComponent, TableComponent],
   imports: [
     CommonModule
