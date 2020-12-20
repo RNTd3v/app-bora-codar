5356e9030ea713f3c59d642c04cdfdbbf7e756ac
Renato Andrade <renato@rntdesign.com.br>
2020-12-20 15:05:20 -0300

    Novos modulos

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -4,19 +4,32 @@ import { SharedModule } from '../shared/shared.module';
 import { UsersComponent } from './users.component';
 import { UserDetailsComponent } from './user-details/user-details.component';
 import { UserEditComponent } from './user-edit/user-edit.component';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
 import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { MatSortModule } from '@angular/material/sort';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [UsersComponent, UserDetailsComponent, UserEditComponent],
   imports: [
     CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatButtonModule,
     MatIconModule,
+    MatInputModule,
     MatTableModule,
     MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
     MatSortModule,
     SharedModule
   ]
