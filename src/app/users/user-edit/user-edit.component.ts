ba0b9dc74bd0c86ae668dca684d0850ca516595a
Renato Andrade <renato@rntdesign.com.br>
2020-12-20 15:05:33 -0300

    Criado componente para editar um usuario

--- src/app/users/user-edit/user-edit.component.ts
+++ src/app/users/user-edit/user-edit.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { IUserService, User } from '@cms/core';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-user-edit',
@@ -7,9 +13,143 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserEditComponent implements OnInit {
 
-  constructor() { }
+  hide = true;
+  formUser: FormGroup;
+  userId: string = null;
+  userData: User;
+  isLoading = false;
+
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private snackBar: MatSnackBar,
+    private service: IUserService) {
+      this.userId = this.activatedRoute.snapshot.params.id;
+    }
 
   ngOnInit(): void {
+
+    if (!!this.userId) {
+      this.userData = JSON.parse(this.activatedRoute.snapshot.queryParams.user);
+    }
+
+    this.createFormUser(new User(this.userData));
   }
 
+  private createFormUser(user: User): void {
+    this.formUser = this.formBuilder.group({
+      name: [user.name, [Validators.required]],
+      email: [user.email, [Validators.required, Validators.email]],
+      passwordForm: this.formBuilder.group({
+        password: [user.passwordForm.password, [Validators.required, Validators.minLength(6)]],
+        confirm: [user.passwordForm.confirm, [Validators.required, Validators.minLength(6)]],
+      }, {
+        validator: ConfirmedValidator('password', 'confirm')
+      }),
+      roleId: [user.roleId]
+    })
+  }
+
+  getErrorMessage(inputName: string) {
+    if (this.formUser.get(inputName).hasError('required')) {
+      return 'Campo obrigatório';
+    }
+
+    switch (inputName) {
+      case 'email':
+        return this.formUser.get(inputName).hasError('email') ? 'E-mail invalido' : '';
+      case 'password':
+        console.log(this.formUser.get(inputName));
+        return this.formUser.get(inputName).hasError('minlength') ? 'Mínimo de 6 caracteres' : '';
+    }
+
+  }
+
+  showMessageError(inputName: string): boolean {
+    return this.formUser.get(inputName).invalid;
+  }
+
+  goBack(): void {
+    this.router.navigate(['../'])
+  }
+
+  submitUserData(): void {
+    if (this.formUser.valid) {
+      this.isLoading = true;
+      this.createOrUpdateUserData()
+      .pipe(
+        finalize(() => this.isLoading = false)
+      ).subscribe(
+        (user: User) => this.handleResult(user),
+        err => this.handleError(err)
+      );
+    }
+  }
+
+  private createOrUpdateUserData(): Observable<User> {
+
+    if(!!this.userData) {
+      return this.updateUserData();
+    }
+
+    return this.createUser();
+  }
+
+  private createUser(): Observable<User> {
+
+    const formValue = this.formUser.value;
+    const { password } = formValue.passwordForm;
+
+    const payload = {
+      ...formValue,
+      password
+    }
+
+    delete payload.passwordForm;
+
+    return this.service.createUser(payload);
+
+  }
+
+  private updateUserData(): Observable<User> {
+    const payload = this.formUser.value;
+    return this.service.updateUser(payload, this.userId);
+  }
+
+  private handleResult(user: User): void {
+    this.snackBar.open('Usuário salvo com sucesso!', null, {
+      duration: 2000,
+    });
+  }
+
+  private handleError(err): void {
+    this.snackBar.open('Houve um erro!', null, {
+      duration: 2000,
+    });
+  }
+
+  get formInvalid(): boolean  {
+    return this.formUser.invalid;
+  }
+
+  get title(): string {
+    return !!this.userData ? this.userData.name : 'Usuários';
+  }
+
+}
+
+export function ConfirmedValidator(controlName: string, matchingControlName: string){
+  return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+      if (matchingControl.errors && !matchingControl.errors.confirmedValidator) {
+          return;
+      }
+      if (control.value !== matchingControl.value) {
+          matchingControl.setErrors({ confirmedValidator: true });
+      } else {
+          matchingControl.setErrors(null);
+      }
+  }
 }
