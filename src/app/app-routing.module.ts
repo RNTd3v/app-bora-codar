7b6d5b10301ece48f98028cf9d957c3da0086e4b
Renato Andrade <renato@rntdesign.com.br>
2020-12-20 15:03:32 -0300

    Add novas rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UserEditComponent } from './users/user-edit/user-edit.component';
 import { UsersComponent } from './users/users.component';
 
 const routes: Routes = [
   {
     path: '',
     component: UsersComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
   },
+  {
+    path: 'users/add',
+    component: UserEditComponent
+  },
+  {
+    path: 'users/:id',
+    component: UserEditComponent
+  }
 ];
 
 @NgModule({
