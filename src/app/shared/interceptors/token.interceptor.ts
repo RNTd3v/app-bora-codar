7d178022f8dad121af914136f8b348ee4e2911a5
Renato Andrade <rntd3signer@gmail.com>
2020-12-21 15:00:33 -0300

    Criado token interceptor

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -3,14 +3,17 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { from, Observable, throwError } from 'rxjs';
+import { IAuthService } from '../services';
+import { catchError, mergeMap } from 'rxjs/operators';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private authService: IAuthService) {}
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
 
@@ -18,7 +21,13 @@ export class TokenInterceptor implements HttpInterceptor {
       return next.handle(this.generateRequestWithoutToken(request));
     }
 
-    return next.handle(request);
+    return this.validateToken().pipe(mergeMap((result: boolean) => {
+      if (result) {
+        return this.handleHttpRequest(next, this.generateRequestWithToken(this.authService.token, request))
+      }
+      return this.handleHttpRequest(next, request);
+    }))
+
   }
 
   private generateRequestWithToken(token: string, request: HttpRequest<any>): HttpRequest<any> {
@@ -31,5 +40,21 @@ export class TokenInterceptor implements HttpInterceptor {
     return request.clone({
       headers: request.headers.delete('no-token')
     })
+  }
+
+  private validateToken(): Observable<boolean> {
+    return from(this.authService.sessionIsValid());
+  }
+
+  private handleHttpRequest(next: HttpHandler, req: HttpRequest<any>): Observable<HttpEvent<any>> {
+    return next.handle(req)
+        .pipe(
+            catchError((error: HttpErrorResponse) => {
+                if (error.status === 401) {
+                    this.authService.logout();
+                }
+                return throwError(error);
+            })
+        );
   }
 }
