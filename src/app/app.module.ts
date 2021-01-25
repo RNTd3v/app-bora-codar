39d797c75c67228b4bf49b5edb7ccedc1e9fa901
Renato Andrade <renato@rntdesign.com.br>
2021-01-25 07:42:38 -0300

    Added snackbar in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,7 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
@@ -29,6 +30,7 @@ import { far } from '@fortawesome/free-regular-svg-icons';
     BrowserAnimationsModule,
     HttpClientModule,
     ThemeModule,
+    MatSnackBarModule,
     FontAwesomeModule
   ],
   providers: [...servicesProviders, ...interceptorProvider],
